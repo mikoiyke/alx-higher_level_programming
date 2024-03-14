1d79f9c3a92e6d5b2c595bb51328d21933ebdffd
mikoiyke <mikoiyke@gmail.com>
2024-03-14 07:29:06 -0700

    Errors fixed

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,6 +1,5 @@
---creates a table called first_table in the current database
-CREATE TABLE IF NOT EXISTS first_table
-(
-		'id' INT,
-		'name' VARCHAR(256)
+-- creates a table called first_table in the current database
+CREATE TABLE first_table (
+		id INT,
+		name VARCHAR(256)
 );
