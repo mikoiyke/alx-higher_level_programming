f84b18401e76a2ffb3ba1b55e4966993e59eadba
mikoiyke <mikoiyke@gmail.com>
2024-04-08 17:07:28 -0700

    5-to_integer.js

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const { argv } = require('node:process');
+
+const convertint = Number.parseInt(argv[2]);
+
+if (isNaN(convertint === undefined || argv.length !== 3)) {
+  console.log('Not a number');
+} else {
+  console.log('My number:', convertint);
+}
