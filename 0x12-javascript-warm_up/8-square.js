8772680b88fd4ca03642aacedf502f126040e4a8
mikoiyke <mikoiyke@gmail.com>
2024-04-08 20:43:33 -0700

    8-square.js

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const { argv } = require('process');
+
+const num = Number.parseInt(argv[2]);
+
+if (isNaN(num)) { 
+  console.log('Missing size');
+} else {
+  for (let x = 0; x < num; x++) {
+    let row = '';
+    for (let j = 0; j < num; j++) {
+      row += 'x';
+    }
+    console.log(row);
+  }
+}
