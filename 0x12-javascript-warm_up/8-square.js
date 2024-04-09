52eb2722b8da8301cef6e66c119620df6bd21cd7
mikoiyke <mikoiyke@gmail.com>
2024-04-08 20:54:48 -0700

    White space removed

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,15 +4,17 @@ const { argv } = require('process');
 
 const num = Number.parseInt(argv[2]);
 
-
+// Check if num is not a number
 if (isNaN(num)) {
   console.log('Missing size');
 } else {
+  // Loop to print rows
   for (let x = 0; x < num; x++) {
     let row = '';
+    // Loop to construct each row
     for (let j = 0; j < num; j++) {
-      row += 'x';
+      row += 'x'; // Add 'x' to the row
     }
-    console.log(row);
+    console.log(row); // Print the row
   }
 }
