acff7391ab2e73b10c222e0a5931312fb32092b2
mikoiyke <mikoiyke@gmail.com>
2024-04-10 19:56:44 -0700

    semistandard errors fixed

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,32 +1,30 @@
-
 #!/usr/bin/node
 
 const { argv } = require('node:process');
-
 const fs = require('fs');
 
-fs.readFile(argv[2], 'utf-8', (errMsg, fileContent) => {
-  if (errMsg) {
-    console.error(errMsg);
-	return -1;
+fs.readFile(argv[2], 'utf-8', (err, fileContent) => {
+  if (err) {
+    console.error(err);
+    return -1; // Note: Returning -1 here won't affect the flow in async callbacks.
   }
-  fs.writeFile(argv[4], fileContent, 'utf-8', (errMsg) => {
-    if (errMsg) {
-      console.error(errMsg);
-	  return -1;
+  fs.writeFile(argv[4], fileContent, 'utf-8', (err) => {
+    if (err) {
+      console.error(err);
+      return -1; // Same note as above.
     }
   });
 });
 
-fs.readFile(argv[3], 'utf-8', (errMsg, fileContent) => {
-  if (errMsg) {
-    console.error(errMsg);
-	return -1;
+fs.readFile(argv[3], 'utf-8', (err, fileContent) => {
+  if (err) {
+    console.error(err);
+    return -1; // Same note as above.
   }
-  fs.appendFile(argv[4], fileContent, 'utf-8', (errMsg) => {
-    if (errMsg) {
-      console.error(errMsg);
-	  return -1;
+  fs.appendFile(argv[4], fileContent, 'utf-8', (err) => {
+    if (err) {
+      console.error(err);
+      return -1; // Same note as above.
     }
   });
 });
