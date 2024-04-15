864feef8378a858c1a117f141af062f9f6bb7775
mikoiyke <mikoiyke@gmail.com>
2024-04-15 16:14:24 -0700

    model_state.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
-"""Module Docs"""
+"""
+Write a script that lists all State objects from the database
+"""
 
-if __name__ == '__ main__':
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
     from sys import argv
     from sqlalchemy import create_engine
     from model_state import Base, State
 
-    db_url = (f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhot:3306/argv[3]"
-             )
+    db_url = f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}"
     engine = create_engine(db_url)
     Session = sessionmaker(bind=engine)
     session = Session()
 
+    # Querying all states ordered by their id
     all_states = session.query(State).order_by(State.id.asc()).all()
-    for state in states:
-        print (f"{state.id}: {state.name}")
+    for state in all_states:
+        print(f"{state.id}: {state.name}")
+
     session.close()
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Y"""
+"""Write a python file that contains the class definition"""
 
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
