6decfc3f9921ee82d0a5b03a8b626af968330ff8
mikoiyke <mikoiyke@gmail.com>
2024-04-15 08:08:02 -0700

    2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+Script to list all states from the database `hbtn_0e_0_usa` that match a specified name
+"""
+
+import MySQLdb
+import sys
+
+def main():
+    if len(sys.argv) != 5:
+        print("Usage: ./script.py username password database_name state_name")
+        return
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Connect to the MySQL database
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name,
+        charset="utf8")
+
+    # Create a cursor object to execute SQL queries
+    cur = db.cursor()
+
+    # SQL query to fetch states that match the given name, sorted by 'id'
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cur.execute(query, (state_name,))
+
+    # Fetch all the results
+    states = cur.fetchall()
+
+    # Print each state from the results
+    for state in states:
+        print(state)
+
+    # Close the cursor and the connection
+    cur.close()
+    db.close()
+
+if __name__ == '__main__':
+    main()
