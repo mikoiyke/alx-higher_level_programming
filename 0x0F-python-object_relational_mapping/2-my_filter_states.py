19882f874ea999739394970c1c3901ee0b37e763
mikoiyke <mikoiyke@gmail.com>
2024-04-15 08:11:07 -0700

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """
-Script to list all states from the database `hbtn_0e_0_usa` that match a specified name
+Script to list all states from the
+database `hbtn_0e_0_usa` that match a specified name
 """
 
 import MySQLdb
 import sys
+
 
 def main():
     if len(sys.argv) != 5:
