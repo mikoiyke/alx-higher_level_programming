6407aaadf2f32d10156c5f79f413b563654606c5
mikoiyke <mikoiyke@gmail.com>
2024-04-26 09:14:55 -0700

    fix line number error

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-# Send a request to the URL and store the response body in a temporary file
 response=$(curl -sS "$1")
-
-# Calculate the size of the response body (in bytes)
-body_size=$(echo "$response" | wc -c)
-
-# Display the size of the response body
-echo "Size of the response body: $body_size bytes"
-
-exit 0
+echo "$response" | wc -c
