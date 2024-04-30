2b84b7f15a3fc717918055f9bb01fe48675797db
mikoiyke <mikoiyke@gmail.com>
2024-04-30 04:44:47 -0700

    1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #GET request to the URL and display the response body (if status code is 200)
-	curl -s -w "\n%{http_code}" "$1" | awk 'END {if ($NF == 200) print $0}'
+curl -sL -w "%{http_code}" -o /dev/null $1
